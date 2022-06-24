d6130d21ca50faca7fcdabb85a64ccaa38747b43
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-06-24 15:57:20 -0400

    added all components in folder and rendered them in index.js

---
+++ components/Main.js
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <main>
+      <p>Hello, I&apos;m</p>
+      <h1>Joseph Fantuzzi</h1>
+      <p>Full Stack Web Developer | Software Engineer</p>
+    </main>
+  );
+};
+
+export default Main;
