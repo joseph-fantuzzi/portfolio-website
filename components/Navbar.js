244e23643a05def435988b2bd0b4a057229bec43
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-06-24 16:52:36 -0400

    started implementing the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
@@ -6,10 +7,11 @@ const Navbar = () => {
 
   return (
     <div className={styles.navcontainer}>
+      <Image width={100} height={100} src="/Logo.png" alt="Logo" />
       {links.map((link) => {
         return (
           <Link key={link} href={"#"}>
-            <a>{link}</a>
+            <a className={link === "Contact" ? styles.contactlink : styles.links}>{link}</a>
           </Link>
         );
       })}
