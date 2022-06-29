24880afeabd4c93902c0af0f6da603ff5f873cd0
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-06-29 16:27:52 -0400

    started the landing page and created scroll icon animation

--- components/Main.js
+++ components/Main.js
@@ -28,9 +28,27 @@ const Main = ({ dark, setDark }) => {
       <div className={styles.togglecontainer}>
         {toggleVisibility && <Toggle dark={dark} setDark={setDark} />}
       </div>
-      <p>Hello, I&apos;m</p>
-      <h1>Joseph Fantuzzi</h1>
-      <p>Full Stack Web Developer | Software Engineer</p>
+      <div className={styles.content}>
+        <div className={styles.intro}>
+          <p className={styles.hello}>Hello, I&apos;m</p>
+          <h1 className={styles.name}>Joseph Fantuzzi</h1>
+          <div className={styles.titlescontainer}>
+            <p className={styles.titles}>Full Stack Web Developer</p>
+            <p className={styles.titles}>Software Engineer</p>
+            <p className={styles.titles}>UI/UX Designer</p>
+          </div>
+          <p className={styles.bio}>
+            Specialized in crafting and implementing modern and beautiful web experiences.
+          </p>
+        </div>
+        <div className={styles.buttons}>
+          <button>Hire Me</button>
+          <button>Resume</button>
+        </div>
+        <div className={styles.animation}>
+          <div className={styles.circle}></div>
+        </div>
+      </div>
     </main>
   );
 };
