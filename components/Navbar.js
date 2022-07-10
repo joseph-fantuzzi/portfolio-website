554f5df6560b8f064c3a07d4749e46b6766c8a03
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-07-09 21:36:36 -0400

    fixed hamburger icon issue when deploying to vercel

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,25 @@
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Navbar.module.css";
 import Logo from "./Logo";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import { Link } from "react-scroll";
 
 const Navbar = ({ setMobileNav }) => {
+  const [hamburgerVisible, setHamburgerVisible] = useState(false);
+
+  function checkMatch() {
+    const x = window.matchMedia("(min-width: 800px)");
+    if (!x.matches) {
+      setHamburgerVisible(true);
+    } else {
+      setHamburgerVisible(false);
+    }
+  }
+
+  useEffect(() => {
+    checkMatch();
+  }, []);
+
   const links = ["About", "Skills", "Work", "Contact"];
 
   return (
@@ -32,11 +48,13 @@ const Navbar = ({ setMobileNav }) => {
             Resume
           </a>
         </div>
-        <MenuRoundedIcon
-          className={styles.hamburger}
-          fontSize="large"
-          onClick={() => setMobileNav(true)}
-        />
+        {hamburgerVisible && (
+          <MenuRoundedIcon
+            className={styles.hamburger}
+            fontSize="large"
+            onClick={() => setMobileNav(true)}
+          />
+        )}
       </div>
     </div>
   );
