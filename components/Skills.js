34abc9605db38b356784449c2b08ac2f24841cef
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-07-11 19:50:07 -0400

    updated the head of the web page

--- components/Skills.js
+++ components/Skills.js
@@ -3,7 +3,7 @@ import styles from "../styles/Skills.module.css";
 const Skills = () => {
   return (
     <div id="skills" className={styles.container}>
-      Skills
+      <i className="devicon-html5-plain colored"></i>
     </div>
   );
 };
