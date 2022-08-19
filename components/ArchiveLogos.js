42d12ec3d3e57994317a7df5c78588fc19ceb648
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-08-18 21:52:26 -0400

    worked more on the archive page

---
+++ components/ArchiveLogos.js
@@ -0,0 +1,276 @@
+import styles from "../styles/ArchiveLogos.module.css";
+
+const PortfolioLogo = () => {
+  return (
+    <svg
+      className={styles.logo}
+      width="102"
+      height="100"
+      viewBox="0 0 102 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <ellipse cx="51" cy="50" rx="51" ry="50" fill="url(#paint0_linear_42_161)" />
+      <g filter="url(#filter0_d_42_161)">
+        <rect
+          x="45.717"
+          y="28"
+          width="7.31541"
+          height="44.8"
+          rx="3.65771"
+          fill="#FFFEFE"
+          fillOpacity="0.75"
+        />
+        <rect
+          x="45.717"
+          y="28"
+          width="28.0424"
+          height="8.96"
+          rx="4.48"
+          fill="white"
+          fillOpacity="0.75"
+        />
+        <rect
+          x="50.594"
+          y="43.36"
+          width="18.2885"
+          height="8.96"
+          rx="4.48"
+          fill="#FFFEFE"
+          fillOpacity="0.75"
+        />
+        <rect
+          x="32.3055"
+          y="65.12"
+          width="20.727"
+          height="7.68"
+          rx="3.84"
+          fill="white"
+          fillOpacity="0.75"
+        />
+        <rect
+          x="32.3055"
+          y="52.32"
+          width="7.31541"
+          height="20.48"
+          rx="3.65771"
+          fill="white"
+          fillOpacity="0.75"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_42_161"
+          x="24.2772"
+          y="28"
+          width="53.4822"
+          height="53"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_42_161" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_42_161"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_42_161"
+          x1="82.9762"
+          y1="10.9375"
+          x2="16.5887"
+          y2="111.861"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#24FF7C" />
+          <stop offset="1" stopColor="#05296F" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+const CryptoxLogo = () => {
+  return (
+    <svg
+      className={styles.logo}
+      width="102"
+      height="100"
+      viewBox="0 0 102 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <ellipse cx="51" cy="50" rx="51" ry="50" fill="url(#paint0_linear_792_1371)" />
+      <g filter="url(#filter0_d_792_1371)">
+        <rect
+          x="35"
+          y="61.1247"
+          width="38"
+          height="5"
+          rx="2.5"
+          transform="rotate(-45.5455 35 61.1247)"
+          fill="white"
+        />
+      </g>
+      <g filter="url(#filter1_d_792_1371)">
+        <rect
+          x="40.6892"
+          y="33"
+          width="38"
+          height="5"
+          rx="2.5"
+          transform="rotate(47.5484 40.6892 33)"
+          fill="white"
+        />
+      </g>
+      <g filter="url(#filter2_d_792_1371)">
+        <rect x="28" y="59" width="38" height="5" rx="2.5" fill="white" />
+      </g>
+      <g filter="url(#filter3_d_792_1371)">
+        <rect x="39" y="34" width="38" height="5" rx="2.5" fill="white" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_792_1371"
+          x="35.0354"
+          y="35.0354"
+          width="30.1113"
+          height="33.5557"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="0.5" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_792_1371" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_792_1371"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_792_1371"
+          x="37.032"
+          y="34.032"
+          width="29.2739"
+          height="34.349"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="0.5" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_792_1371" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_792_1371"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter2_d_792_1371"
+          x="27"
+          y="59"
+          width="40"
+          height="10"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="0.5" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_792_1371" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_792_1371"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter3_d_792_1371"
+          x="38"
+          y="34"
+          width="40"
+          height="10"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="0.5" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_792_1371" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_792_1371"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_792_1371"
+          x1="82.9762"
+          y1="10.9375"
+          x2="15.4407"
+          y2="80.9171"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#52E6FA" />
+          <stop offset="1" stopColor="#07112F" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+const ArchiveLogos = { PortfolioLogo, CryptoxLogo };
+
+export default ArchiveLogos;
--- pages/archive.js
+++ pages/archive.js
@@ -1,13 +1,17 @@
 import Footer from "../components/Footer";
 import styles from "../styles/Archive.module.css";
 import data from "../utils/archive-data";
-import { FiFolder } from "react-icons/fi";
+import ArchiveLogos from "../components/ArchiveLogos";
+import Socials from "../components/Socials";
+import { FiFolder, FiExternalLink, FiGithub } from "react-icons/fi";
 
 const archive = () => {
   let isDark;
   if (typeof window !== "undefined") {
     isDark = window.localStorage.getItem("dark");
   }
+
+  const labels = ["", "Year", "Name", "Category", "Technologies", "Link"];
 
   return (
     <div className={styles.full}>
@@ -18,40 +22,109 @@ const archive = () => {
             <p className={styles.about}>A record of the work I&apos;ve done.</p>
           </div>
           <div className={styles.archive}>
-            <div className={styles.personal}>
+            <div className={styles.labelcontainer}>
+              {labels.map((label, i) => {
+                if (label === "Category" || label === "Technologies") {
+                  return (
+                    <div
+                      className={label === "Category" ? styles.categorylabel : styles.techlabel}
+                      key={i}
+                    >
+                      {label}
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div className={styles.label} key={i}>
+                      {label}
+                    </div>
+                  );
+                }
+              })}
+            </div>
+            <div className={styles.projectcontainer}>
               {data.personalProjects.map((project, i) => {
                 return (
                   <div key={i} className={styles.project}>
-                    <FiFolder fontSize={22} />
-                    <h1>{project.name}</h1>
+                    {project.name === "Portfolio-v1" ? (
+                      <ArchiveLogos.PortfolioLogo />
+                    ) : project.name === "Cryptox" ? (
+                      <ArchiveLogos.CryptoxLogo />
+                    ) : (
+                      <FiFolder fontSize={22} />
+                    )}
+                    <p className={styles.date}>{project.date}</p>
+                    <h1 className={styles.name}>{project.name}</h1>
+                    <p className={styles.category}>Personal</p>
+                    <div className={styles.technologies}>
+                      {project.technologies.map((tech, i) => {
+                        return <p key={i}>{tech}</p>;
+                      })}
+                    </div>
+                    <div className={styles.link}>
+                      <a href={project.site} className={styles.site}>
+                        <FiExternalLink fontSize={22} />
+                      </a>
+                      <a href={project.github} className={styles.github}>
+                        {project.name === "Portfolio-v0" ? "" : <FiGithub fontSize={22} />}
+                      </a>
+                    </div>
                   </div>
                 );
               })}
             </div>
-            <div className={styles.bloomtech}>
+            <div className={styles.apprenticeshipcontainer}>
               {data.bloomtechPRs.map((pr, i) => {
                 return (
                   <div key={i} className={styles.project}>
                     <FiFolder fontSize={22} />
-                    <h1>{pr.name}</h1>
+                    <p className={styles.date}>{pr.date}</p>
+                    <h1 className={styles.name}>{pr.name}</h1>
+                    <p className={styles.category}>BloomTech Apprenticeship</p>
+                    <div className={styles.technologies}></div>
+                    <div className={styles.link}>
+                      <a href={pr.link} className={styles.github}>
+                        <FiGithub fontSize={22} />
+                      </a>
+                    </div>
                   </div>
                 );
               })}
+            </div>
+            <div className={styles.bloomtechprojectcontainer}>
               {data.bloomtechProjects.map((project, i) => {
                 return (
                   <div key={i} className={styles.project}>
                     <FiFolder fontSize={22} />
-                    <h1>{project.name}</h1>
+                    <p className={styles.date}>{project.date}</p>
+                    <h1 className={styles.name}>{project.name}</h1>
+                    <p className={styles.category}>BloomTech Projects</p>
+                    <div className={styles.technologies}></div>
+                    <div className={styles.link}>
+                      <a href={project.github} className={styles.github}>
+                        <FiGithub fontSize={22} />
+                      </a>
+                    </div>
                   </div>
                 );
               })}
+            </div>
+            <div className={styles.sprintcontainer}>
               {data.bloomtechSprints.map((sprintChallenge, i) => {
                 return (
                   <div key={i} className={styles.project}>
                     <FiFolder fontSize={22} />
-                    <h1>
+                    <p className={styles.date}>{sprintChallenge.date}</p>
+                    <h1 className={styles.name}>
                       Sprint Challenge {i + 1}: {sprintChallenge.name}
                     </h1>
+                    <p className={styles.category}>BloomTech Sprint Challenges</p>
+                    <div className={styles.technologies}></div>
+                    <div className={styles.link}>
+                      <a href={sprintChallenge.link} className={styles.github}>
+                        <FiGithub fontSize={22} />
+                      </a>
+                    </div>
                   </div>
                 );
               })}
@@ -60,6 +133,7 @@ const archive = () => {
         </div>
       </div>
       <Footer />
+      <Socials />
     </div>
   );
 };
